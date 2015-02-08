455606dec56e380ed6da7e67f67fb9ff66e33ffc
beLive <ndenisenko@wayray.com>
2015-02-08 16:44:52 +0300

    Word Break

--- algorithm/src/com/be1ive/leetcode/WordBreak.java
+++ algorithm/src/com/be1ive/leetcode/WordBreak.java
@@ -7,19 +7,19 @@ import java.util.Set;
  */
 public class WordBreak {
 
+    //go bottom up checking whether new letter gives word break that exists in dictionary
+    //when adding new letter we should check all possible combinations
+    //
+    //Ex.
+    //When adding i-th letter "c" to "ode" we should check "code" + "", "co" + "de", "cod" + "e"
+    //If any combination exists then d[i] = true;
+    //
     public boolean wordBreak(String s, Set<String> dict) {
         int length = s.length();
 
         boolean[] dp = new boolean[length + 1];
         dp[length] = true;
 
-        //go bottom up checking whether new letter gives word break that exists in dictionary
-        //when adding new letter we should check all possible combinations
-        //
-        //Ex.
-        //When adding i-th letter "c" to "ode" we should check "code" + "", "co" + "de", "cod" + "e"
-        //If any combination exists then d[i] = true;
-        //
         for (int i = length - 1; i >= 0; i--) {
             for (int j = i + 1; j <= length; j++) {
                 if (dict.contains(s.substring(i, j)) && dp [j]) {
